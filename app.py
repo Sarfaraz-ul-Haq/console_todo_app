0b3f06a55f7c8c48e8834f4341bcf08627cc3624
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-09-10 16:46:42 +0500

    write condition to check empty string or duplicate todos

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from typing import Any, List, Dict, Union
 
-# List[Dict[str, Union[str, bool]]]
-
 todo_list = []
 
 while True:
@@ -9,7 +7,18 @@ while True:
 
     if todo.lower() == "exit":
         break
+
+    if todo.strip() == "":
+        print("Please enter a todo: ")
+        continue
+
+    for todo_dict in todo_list:
+        if todo_dict["todo"] == todo:
+            add = input("Duplicate todo! Do you still want to add it? (yes / no)")
+            if add.lower() == "no":
+                break
     else:
         todo_list.append({"todo": todo, "completed": False})
+
 
 print(todo_list)
