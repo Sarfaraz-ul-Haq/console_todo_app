48fcc8dba5f5e33edf49861bf19dd9f54482b623
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-09-23 23:22:26 +0500

    create delete_todo_item() function and update main function()

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def display_todo_list(todo_list: List[ToDoItem]) -> None:
     if not todo_list:
         print("Your Todo list is empty!")
         return
-
-    print(f"\nHere is your todo list:\n")
+    
+    print(f"Here is your todo list:\n")
     for id, todo_item in enumerate(todo_list, start=1):
         if todo_item["is_completed"]:
             status: str = "Done"
@@ -55,6 +55,9 @@ def display_todo_list(todo_list: List[ToDoItem]) -> None:
             status: str = "Pending"
 
         print(f"{id}. Todo: {todo_item["todo"]} - Description: {todo_item["description"]} - [{status}]")
+
+    print("_________________________________________________________________________________________________________\n")
+
 
 
 def add_todo_item(to_do_list: List[ToDoItem], todo: str, description: str) -> None:
@@ -72,7 +75,15 @@ def get_task_number(action: str) -> int:
         except ValueError:
             print("\n Invalid input. Please enter a valid number.")
 
+def delete_todo_item(index: int) -> None:
+        index -= 1
+        if index >= 0 and index < len(todo_list):
+            todo_list.pop(index)
+            print("\nTodo deleted successfully")
+        else:
+            print("\nInvalid  todo number. Please try again.")
 
+    
 # add_todo(todo_list, "Learn Transformers")
 
 # display_todos(todo_list)
@@ -80,9 +91,10 @@ def get_task_number(action: str) -> int:
 
 def main() -> None:
     while True:
-        print("\nTo-Do List Options:\n")
-        print("1. Display todos")
-        print("2. Add todo")
+        print("\nTodo List Options:")
+        print("==================\n")
+        print("1. Display todo list")
+        print("2. Add a todo")
         print("3. Update todo")
         print("4. Mark todo as done")
         print("5. Delete todo")
@@ -94,7 +106,7 @@ def main() -> None:
             display_todo_list(todo_list)
         elif choice == "2":
             todo: str = input("Enter your todo: ").strip()
-            description: str = input("Enter  todo description: ").strip()
+            description: str = input("Enter todo description: ").strip()
             if todo.strip() != "" and description.strip() != "":
                 add_todo_item(todo_list, todo, description)
             else:
@@ -104,7 +116,12 @@ def main() -> None:
         elif choice == "4":
             pass
         elif choice == "5":
-            pass
+            try:
+                todo_number: int = int(input("Enter the number of the todo to delete: "))
+                delete_todo_item(todo_number)
+            except ValueError:
+                print("\nInvalid input. Please enter a number.")
+
         elif choice == "6":
             pass
 
